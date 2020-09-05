715fec0c779dd360fa67be921e13b4844813a842
Grant <gmontgomery1994@gmail.com>
2020-09-05 16:20:35 -0700

    new deploy.

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Layout } from "../components";
 
 export default function About() {
+  console.log("dest deploy");
   return (
     <React.Fragment>
       <Layout></Layout>
