944e050904761cfe28d88a544b6462439ef87386
Grant <gmontgomery1994@gmail.com>
2020-09-07 23:09:46 -0700

    account display component setup.

--- components/AccountDisplay/AccountDisplay.tsx
+++ components/AccountDisplay/AccountDisplay.tsx
@@ -2,5 +2,12 @@ import * as React from "react";
 import css from "./AccountDisplay.module.scss";
 
 export const AccountDisplay: React.FC = () => {
-  return <div className={css.accountDisplay}></div>;
+  const [state, setState] = React.useState<{ loggedIn: boolean }>({
+    loggedIn: false,
+  });
+  return (
+    <div className={css.accountDisplay}>
+      <span> {state.loggedIn ? "Grant" : "Sign In"}</span>
+    </div>
+  );
 };
