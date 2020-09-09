c6808cb95ab386f7e2d1cb38aaf59f159197f0d3
Grant <gmontgomery1994@gmail.com>
2020-09-08 21:01:56 -0700

    some issues loading css from datepicker again.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "react-calendar/dist/Calendar.css";
+// import "react-calendar/dist/Calendar.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-datepicker/dist/react-datepicker.min.css";
 
