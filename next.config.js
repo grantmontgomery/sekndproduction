a48ac8fc88214ea2bb789f7b9487489da978914a
Grant <gmontgomery1994@gmail.com>
2020-09-13 13:27:16 -0700

    setting up serverless functionality and lambda functions.

--- next.config.js
+++ next.config.js
@@ -9,4 +9,12 @@ module.exports = {
     YELP_API_KEY: process.env.YELP_API_KEY,
     TICKETMASTER_API_KEY: process.env.TICKETMASTER_API_KEY,
   },
+  target: "serverless",
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: "empty",
+    };
+    return config;
+  },
 };
