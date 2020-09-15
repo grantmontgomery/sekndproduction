b5e5d315b60fa753f23460a4f390361c7b028c21
Grant <gmontgomery1994@gmail.com>
2020-09-15 14:56:04 -0700

    buttons for sorting created.

--- pages/search/[queried].tsx
+++ pages/search/[queried].tsx
@@ -7,30 +7,74 @@ import {
 } from "../../apicalls";
 import css from "../../styles/Queried.module.scss";
 
-export default function Queried({ results }): JSX.Element {
-  console.log(results);
-  if (results) {
-    return (
-      <Layout>
-        <main className={css.queriedPage}>
-          <section className={css.queryDisplay}></section>
-          <section className={css.header}></section>
-          <section className={css.results}>
-            <div className={css.resultsSlider}>
-              {typeof results === "object"
-                ? results.map((item) => {
-                    return <ResultCard key={item.id} item={item}></ResultCard>;
-                  })
-                : null}
-            </div>
-          </section>
-        </main>
-      </Layout>
-    );
-  }
+export default function Queried({ results, searchType }): JSX.Element {
+  const [state, setState] = React.useState({ resultsType: "" });
+  React.useEffect(() => {
+    if (searchType) {
+      switch (searchType) {
+        case "ALL":
+      }
+    }
+  });
+
+  const setTypeButtons: () => JSX.Element = () => {
+    if (searchType) {
+      switch (searchType) {
+        case "ALL":
+          return (
+            <React.Fragment>
+              <button className={css.typeButton}>Places</button>
+              <button className={css.typeButton}>Events</button>
+            </React.Fragment>
+          );
+        case "PLACES":
+          return <button className={css.typeButton}>Places</button>;
+
+        case "EVENTS":
+          return <button className={css.typeButton}>Events</button>;
+        default:
+          return (
+            <React.Fragment>
+              <button className={css.typeButton}>Places</button>
+              <button className={css.typeButton}>Events</button>
+            </React.Fragment>
+          );
+      }
+    } else {
+      return (
+        <React.Fragment>
+          <button className={css.typeButton}>Places</button>
+          <button className={css.typeButton}>Events</button>
+        </React.Fragment>
+      );
+    }
+  };
+
+  return (
+    <Layout>
+      <main className={css.queriedPage}>
+        <section className={css.queryDisplay}></section>
+        <section className={css.header}>
+          {setTypeButtons()}
+          <button className={css.sort}>Sort</button>
+        </section>
+        <section className={css.results}>
+          <div className={css.resultsSlider}>
+            {/* {results
+              ? results.map((item) => {
+                  return <ResultCard key={item.id} item={item}></ResultCard>;
+                })
+              : null} */}
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
 }
 
-Queried.getInitialProps = async ({ query }): Promise<{ results: any }> => {
+Queried.getInitialProps = async ({
+  query,
+}): Promise<{ results: any; searchType: string }> => {
   if (query.queried) {
     const checkURLIsString: string = query.queried.toString();
     const paramValueArray: string[] = checkURLIsString.split("+");
@@ -51,8 +95,8 @@ Queried.getInitialProps = async ({ query }): Promise<{ results: any }> => {
       { [key: string]: any }[] | string
     > = await yelpBusinessesCall(searchParamsValues);
 
-    return { results: apiResponse };
+    return { results: apiResponse, searchType: searchParamsValues.searchType };
   } else {
-    return { results: {} };
+    return { results: {}, searchType: "ALL" };
   }
 };
