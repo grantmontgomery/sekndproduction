608a65c359a39502d232c5048f1d759f84f663c5
Grant <gmontgomery1994@gmail.com>
2020-09-25 12:38:10 -0700

    date parts color.

--- components/DateParts/DateParts.tsx
+++ components/DateParts/DateParts.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
-import { usePartsState } from "../../state/DatePartsContext";
+import { usePartsState, usePartsDispatch } from "../../state/DatePartsContext";
 import css from "./DateParts.module.scss";
 import { DatePart } from "../DatePart/DatePart";
 export const DateParts: React.FC<{ location: string }> = ({ location }) => {
   const { parts } = usePartsState();
+  const [state, setState] = React.useState<{ color: string }>({
+    color: "linear-gradient(45deg, #155799, #159957)",
+  });
+
   return (
     <div className={`${css.dateParts} ${css[location]}`}>
       <div className={css.partsWrapper}>
@@ -15,13 +19,38 @@ export const DateParts: React.FC<{ location: string }> = ({ location }) => {
             className={css.partInput}
           />
           <div className={css.colorInput}>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
+            <button
+              onClick={() =>
+                setState({ color: "linear-gradient(45deg, #155799, #159957)" })
+              }
+            ></button>
+            <button
+              onClick={() =>
+                setState({ color: "linear-gradient(45deg, #ee0979, #ff6a00)" })
+              }
+            ></button>
+            <button
+              onClick={() =>
+                setState({ color: "linear-gradient(45deg, #799f0c, #ffe000)" })
+              }
+            ></button>
+            <button
+              onClick={() =>
+                setState({ color: "linear-gradient(45deg, #333399, #ff00cc)" })
+              }
+            ></button>
+            <button
+              onClick={() =>
+                setState({ color: "linear-gradient(45deg, #0f2027, #2c5364)" })
+              }
+            ></button>
           </div>
-          <button className={css.add}>Add</button>
+          <button
+            className={css.add}
+            style={{ color: "white", background: state.color }}
+          >
+            Add
+          </button>
         </header>
         <div className={css.partsList}>
           <div className={css.partsInner}>
