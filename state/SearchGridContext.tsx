95be92242a7288ad8f6aff562bd7cb58daa9b7b2
Grant <gmontgomery1994@gmail.com>
2020-10-01 21:25:23 -0700

    setting up types for grid context.

--- state/SearchGridContext.tsx
+++ state/SearchGridContext.tsx
@@ -10,18 +10,21 @@ type GridTimes = {
 };
 
 type State = {
-  squares: Squares
-} & GridTimes
+  squares: Squares;
+} & GridTimes;
 
 type Action = {
   type: string;
   payload: {
-    id?: string,
-    squares?:number,
-    startTime?:string,
-    
-  }
-}
+    id?: string;
+    index?: number;
+    squares?: number;
+    startDate?: string;
+    startTime?: number;
+    endDate?: string;
+    endTime?: number;
+  };
+};
 
 //squares for the grid
 //grid template string
@@ -29,4 +32,24 @@ type Action = {
 //grid template width?
 //grid start and end dates
 
-const gridReducer:React.Reducer<State, 
+const gridReducer: React.Reducer<State, Action> = (state: State, action) => {
+  switch (action.type) {
+    case "ADD_SQUARES":
+      return { ...state, squares: action.payload.squares };
+    case "ADD_TIMES":
+      return {
+        ...state,
+        startTime: action.payload.startTime,
+        startDate: action.payload.startDate,
+        endTime: action.payload.endTime,
+        endDate: action.payload.endDate,
+      };
+    case "ADD_PART_TO_SQUARE":
+      return {
+        ...state,
+        squares: state.squares.map((square, index) => index),
+      };
+    default:
+      return state;
+  }
+};
