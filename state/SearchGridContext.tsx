4eb699e5eda62f9771c2d0dd093267fb34ac147f
Grant <gmontgomery1994@gmail.com>
2020-10-02 13:06:03 -0700

    more context added.

--- state/SearchGridContext.tsx
+++ state/SearchGridContext.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-type Squares = { part: JSX.Element }[];
+type Squares = { part: { [key: string]: any } | null }[];
 
 type GridTimes = {
   startTime: number;
@@ -18,11 +18,12 @@ type Action = {
   payload: {
     id?: string;
     index?: number;
-    squares?: number;
+    numberOfSquares?: number;
     startDate?: string;
     startTime?: number;
     endDate?: string;
     endTime?: number;
+    part?: { [key: string]: any };
   };
 };
 
@@ -35,7 +36,12 @@ type Action = {
 const gridReducer: React.Reducer<State, Action> = (state: State, action) => {
   switch (action.type) {
     case "ADD_SQUARES":
-      return { ...state, squares: action.payload.squares };
+      const squares: Squares = [];
+      let numberOfSquares: number = 0;
+      while (numberOfSquares < action.payload.numberOfSquares) {
+        squares.push({ part: null });
+      }
+      return { ...state, squares };
     case "ADD_TIMES":
       return {
         ...state,
@@ -47,9 +53,68 @@ const gridReducer: React.Reducer<State, Action> = (state: State, action) => {
     case "ADD_PART_TO_SQUARE":
       return {
         ...state,
-        squares: state.squares.map((square, index) => index),
+        squares: state.squares.map((square, i) => {
+          return i !== action.payload.index
+            ? square
+            : { part: action.payload.part };
+        }),
+      };
+    case "REMOVE_PART_FROM_SQUARE":
+      return {
+        ...state,
+        squares: state.squares.map((square, i) => {
+          return i !== action.payload.index
+            ? { part: action.payload.part }
+            : square;
+        }),
       };
     default:
       return state;
   }
 };
+
+const GridStateContext: React.Context<State> = React.createContext(
+  undefined
+);
+
+const GridDispatchContext: React.Context<
+  React.Dispatch<Action> | undefined
+> = React.createContext(undefined);
+
+export const PartsProvider: ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => JSX.Element = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = React.useReducer(gridReducer, {
+    squares: [],
+    startDate: "",
+    startTime: 0,
+    endDate: "",
+    endTime: 0,
+  });
+
+  return (
+    <GridStateContext.Provider value={state}>
+      <GridDispatchContext.Provider value={dispatch}>
+        {children}
+      </GridDispatchContext.Provider>
+    </GridStateContext.Provider>
+  );
+};
+
+export const useGridState = (): State => {
+  const context = React.createContext(GridStateContext);
+
+  if(undefined === context){
+    throw new Error("Please use within GridProvider");
+  }
+  return context
+};
+
+
+export const useGridDispatch = ():React.Dispatch<Action> => {
+  const context = React.useContext(GridDispatchContext)
+
+  if(undefined === )
+}
