2732599f53748774a7b1dbb535a70f5653309030
Grant <gmontgomery1994@gmail.com>
2020-10-02 20:24:30 -0700

    adding a function which dispatches square number.

--- components/SearchBox/Logic/setScheduleGrid.ts
+++ components/SearchBox/Logic/setScheduleGrid.ts
@@ -1,3 +1,4 @@
+import { Dispatch } from "react";
 import { useGridDispatch } from "../../../state/SearchGridContext";
 
 class ScheduleGrid {
@@ -22,11 +23,23 @@ class ScheduleGrid {
   public setSquares() {
     const timeDifference: number = this.endTime - this.startTime;
     const hours: number = Math.floor(timeDifference / 3600000);
-
     const numColumns: number = hours * 2 + 4;
     const numSquares: number = numColumns * 5;
     this.numberofSquares = numSquares;
   }
 
   public name() {}
+}
+
+const dispatch = useGridDispatch();
+
+export default function setSquares(
+  unixStartDate: number,
+  unixEndDate: number
+): void {
+  const timeDifference: number = unixEndDate - unixStartDate;
+  const hours: number = Math.floor(timeDifference / 3600000);
+  const numColumns: number = hours * 2 + 4;
+  const numberOfSquares: number = numColumns * 5;
+  return dispatch({ type: "ADD_SQUARES", payload: { numberOfSquares } });
 }
