c52deffe9948b4653279bbd67f75b4ff1b7d63c1
Grant <gmontgomery1994@gmail.com>
2020-10-06 15:55:11 -0700

    took out out folder.

--- out/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/search/[queried]":["static\u002Fchunks\u002Fpages\u002Fsearch\u002F[queried].js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fsearch\u002F[queried]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
