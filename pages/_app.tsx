cc0929af797701bd65e85c3b0544402764e0db6c
Grant <gmontgomery1994@gmail.com>
2020-10-06 16:29:23 -0700

    changed custom app in to a function instead of a class to solve typescript errors.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-datepicker/dist/react-datepicker.min.css";
-import App from "next/app";
 import { PageTransition } from "next-page-transitions";
 import Cookie from "js-cookie";
 import { SekndLoader } from "../components";
@@ -10,6 +9,8 @@ import { GridProvider } from "../state/SearchGridContext";
 import { SquaresProvider } from "../state/GridSquaresContext";
 
 import { parseCookies } from "../cookies/parseCookies";
+import { NextComponentType, NextPage, NextPageContext } from "next";
+import { NextRouter } from "next/router";
 
 type InitialSquaresState = {
   squares: { part: { [key: string]: any } | null }[];
@@ -17,43 +18,28 @@ type InitialSquaresState = {
 type InitialGridState = { gridTemplate: string; hourStrings: string[] };
 type InitialPartsState = { parts: { [key: string]: any }[] | [] };
 
-class MyApp extends App<{
-  parts: { [key: string]: any }[];
-  gridTemplate: string;
-  hourStrings: string[];
-  squares: { part: null | { [key: string]: any }[] };
-  initialSquaresState?: InitialSquaresState;
-  initialGridState?: InitialGridState;
-  initialPartsState?: InitialPartsState;
-}> {
-  static async getInitialProps({ ctx: req, Component }) {
-    const { req: actualRequest } = req;
+type Props = {
+  Component: NextComponentType;
+  pageProps: NextPageContext;
+  router: NextRouter;
+  initialSquaresState: InitialSquaresState;
+  initialGridState: InitialGridState;
+  initialPartsState: InitialPartsState;
+};
 
-    const cookieObject: { [key: string]: string } = parseCookies(actualRequest);
-    if (cookieObject === {}) return {};
-    const { parts, grid, squares } = cookieObject;
-
-    const initialSquaresState: InitialSquaresState = JSON.parse(squares);
-    const initialPartsState: InitialPartsState = JSON.parse(parts);
-    const initialGridState: InitialGridState = JSON.parse(grid);
-
-    return { initialSquaresState, initialPartsState, initialGridState };
-  }
-
-  render() {
-    const {
-      Component,
-      pageProps,
-      router,
-      initialSquaresState,
-      initialGridState,
-      initialPartsState,
-    } = this.props;
-    return (
-      <SquaresProvider initialSquaresState={initialSquaresState}>
-        <GridProvider initialGridState={initialGridState}>
-          <PartsProvider initialPartsState={initialPartsState}>
-            {/* <PageTransition
+export default function App({
+  Component,
+  pageProps,
+  router,
+  initialSquaresState,
+  initialGridState,
+  initialPartsState,
+}: Props): JSX.Element {
+  return (
+    <SquaresProvider initialSquaresState={initialSquaresState}>
+      <GridProvider initialGridState={initialGridState}>
+        <PartsProvider initialPartsState={initialPartsState}>
+          {/* <PageTransition
           timeout={400}
           loadingComponent={<SekndLoader></SekndLoader>}
           loadingDelay={500}
@@ -64,43 +50,54 @@ class MyApp extends App<{
           classNames="page-transition"
           loadingClassNames="loading-indicator"
         > */}
-            <Component {...pageProps} key={router.route} />
-            {/* </PageTransition> */}
-            <style jsx global>{`
-              .page-transition-enter {
-                opacity: 0;
-              }
-              .page-transition-enter-active {
-                opacity: 1;
-                transition: opacity 300ms;
-              }
-              .page-transition-exit {
-                opacity: 1;
-              }
-              .page-transition-exit-active {
-                opacity: 0;
-                transition: opacity 300ms;
-              }
-              .loading-indicator-enter {
-                opacity: 0;
-              }
-              .loading-indicator-enter-active {
-                opacity: 1;
-                transition: opacity 300ms;
-              }
-              .loading-indicator-exit {
-                opacity: 1;
-              }
-              .loading-indicator-exit-active {
-                opacity: 0;
-                transition: opacity 300ms;
-              }
-            `}</style>
-          </PartsProvider>
-        </GridProvider>
-      </SquaresProvider>
-    );
-  }
+          <Component {...pageProps} key={router.route} />
+          {/* </PageTransition> */}
+          <style jsx global>{`
+            .page-transition-enter {
+              opacity: 0;
+            }
+            .page-transition-enter-active {
+              opacity: 1;
+              transition: opacity 300ms;
+            }
+            .page-transition-exit {
+              opacity: 1;
+            }
+            .page-transition-exit-active {
+              opacity: 0;
+              transition: opacity 300ms;
+            }
+            .loading-indicator-enter {
+              opacity: 0;
+            }
+            .loading-indicator-enter-active {
+              opacity: 1;
+              transition: opacity 300ms;
+            }
+            .loading-indicator-exit {
+              opacity: 1;
+            }
+            .loading-indicator-exit-active {
+              opacity: 0;
+              transition: opacity 300ms;
+            }
+          `}</style>
+        </PartsProvider>
+      </GridProvider>
+    </SquaresProvider>
+  );
 }
 
-export default MyApp;
+App.getInitialProps = async ({ ctx: req }) => {
+  const { req: actualRequest } = req;
+
+  const cookieObject: { [key: string]: string } = parseCookies(actualRequest);
+  if (cookieObject === {}) return {};
+  const { parts, grid, squares } = cookieObject;
+
+  const initialSquaresState: InitialSquaresState = JSON.parse(squares);
+  const initialPartsState: InitialPartsState = JSON.parse(parts);
+  const initialGridState: InitialGridState = JSON.parse(grid);
+
+  return { initialSquaresState, initialPartsState, initialGridState };
+};
