bc1d2eb0f8560ba3ca39371a767b7697c3757c98
Grant <gmontgomery1994@gmail.com>
2020-10-06 15:46:10 -0700

    took out next. still need to figure out typescript error in App.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,64 +9,7 @@ import { PartsProvider } from "../state/DatePartsContext";
 import { GridProvider } from "../state/SearchGridContext";
 import { SquaresProvider } from "../state/GridSquaresContext";
 
-import { Component } from "react";
 import { parseCookies } from "../cookies/parseCookies";
-import { JS } from "aws-amplify";
-import { type } from "os";
-
-// function MyApp({ Component, pageProps, router }) {
-//   return (
-//     <SquaresProvider>
-//       <GridProvider>
-//         <PartsProvider>
-//           {/* <PageTransition
-//           timeout={400}
-//           loadingComponent={<SekndLoader></SekndLoader>}
-//           loadingDelay={500}
-//           loadingTimeout={{
-//             enter: 500,
-//             exit: 0,
-//           }}
-//           classNames="page-transition"
-//           loadingClassNames="loading-indicator"
-//         > */}
-//           <Component {...pageProps} key={router.route} />
-//           {/* </PageTransition> */}
-//           <style jsx global>{`
-//             .page-transition-enter {
-//               opacity: 0;
-//             }
-//             .page-transition-enter-active {
-//               opacity: 1;
-//               transition: opacity 300ms;
-//             }
-//             .page-transition-exit {
-//               opacity: 1;
-//             }
-//             .page-transition-exit-active {
-//               opacity: 0;
-//               transition: opacity 300ms;
-//             }
-//             .loading-indicator-enter {
-//               opacity: 0;
-//             }
-//             .loading-indicator-enter-active {
-//               opacity: 1;
-//               transition: opacity 300ms;
-//             }
-//             .loading-indicator-exit {
-//               opacity: 1;
-//             }
-//             .loading-indicator-exit-active {
-//               opacity: 0;
-//               transition: opacity 300ms;
-//             }
-//           `}</style>
-//         </PartsProvider>
-//       </GridProvider>
-//     </SquaresProvider>
-//   );
-// }
 
 type InitialSquaresState = {
   squares: { part: { [key: string]: any } | null }[];
