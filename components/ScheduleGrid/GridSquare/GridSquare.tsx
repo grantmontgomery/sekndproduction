756b4b43d28b3da1093f960d30ac227e69fa7cc0
Grant <gmontgomery1994@gmail.com>
2020-10-07 11:55:10 -0700

    attached grid states to schedule grid.

--- components/ScheduleGrid/GridSquare/GridSquare.tsx
+++ components/ScheduleGrid/GridSquare/GridSquare.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import css from "./GridSquare.module.scss";
 
-export const GridSquare: React.FC = () => {
+export const GridSquare: React.FC<{ part: { [key: string]: any } | null }> = ({
+  part,
+}) => {
   return <div className={css.square}></div>;
 };
--- components/ScheduleGrid/ScheduleGrid.tsx
+++ components/ScheduleGrid/ScheduleGrid.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
+import { GridSquare } from "./GridSquare";
+import { useSquaresState } from "../../state/GridSquaresContext";
+import { useGridState } from "../../state/SearchGridContext";
 import css from "./ScheduleGrid.module.scss";
+import { spawn } from "child_process";
 
 export const ScheduleGrid: React.FC = () => {
+  const { squares } = useSquaresState();
+  const { hourStrings, gridTemplate } = useGridState();
+
   return (
     <div className={css.gridWrapper}>
-      <div className={css.innerGrid}></div>
+      <div className={css.innerGrid}>
+        {squares && squares.length > 0 ? (
+          squares.map((square, index) => (
+            <GridSquare
+              key={`gridSquare${index}`}
+              part={square.part}
+            ></GridSquare>
+          ))
+        ) : (
+          <h1>Make a search!</h1>
+        )}
+      </div>
     </div>
   );
 };
