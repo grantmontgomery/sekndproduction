ba2c2814dd76a23d6d2ec411640a34afe4bed31c
Grant <gmontgomery1994@gmail.com>
2020-10-14 14:58:36 -0700

    elementbelow being set.

--- components/ScheduleDragPiece/ScheduleDragPiece.tsx
+++ components/ScheduleDragPiece/ScheduleDragPiece.tsx
@@ -1,5 +1,6 @@
 import { a } from "aws-amplify";
 import * as React from "react";
+import useInterval from "./useInterval";
 import css from "./ScheduleDragPiece.module.scss";
 
 export const ScheduleDragPiece: React.FC<{ part?: { [key: string]: any } }> = ({
@@ -11,33 +12,59 @@ export const ScheduleDragPiece: React.FC<{ part?: { [key: string]: any } }> = ({
     translation: { y: number };
     initialScrollTop: number;
     moveScroll: boolean;
+    elementBelow: Element | null;
+    draggingElement: any | null;
   }>({
     isDragging: false,
     moveScroll: false,
     origin: { y: 0 },
     translation: { y: 0 },
     initialScrollTop: 0,
+    elementBelow: null,
+    draggingElement: null,
   });
   const [height, extend] = React.useState<string>("10vh");
 
   const handleTouchStart = ({ touches, currentTarget, target }): void => {
-    const { clientY } = touches[0];
+    const { clientY, clientX } = touches[0];
     const initialScrollTop: number = document.getElementById("innerGrid")
       .scrollTop;
+
+    currentTarget.hidden = true;
+    const elementBelow: Element = document.elementFromPoint(clientX, clientY);
+    currentTarget.hidden = false;
+
+    console.log(elementBelow);
 
     setPosition((dragPosition) => ({
       ...dragPosition,
       isDragging: true,
       initialScrollTop,
+      draggingElement: currentTarget,
       origin: { y: clientY },
+      elementBelow,
     }));
   };
 
   const handleTouchMove = React.useCallback(
-    ({ touches }: TouchEvent): void => {
+    ({ touches, currentTarget, target }): void => {
       if (window) {
         if (dragPosition.isDragging) {
-          const { clientY } = touches[0];
+          const { clientY, clientX } = touches[0];
+          dragPosition.draggingElement.hidden = true;
+          dragPosition.draggingElement.childNodes[0].hidden = true;
+          // dragPosition.draggingElement.childNodes[0].forEach((element) => {
+          //   element.hidden = true;
+          // });
+          const elementBelow: Element = document.elementFromPoint(
+            clientX,
+            clientY
+          );
+          dragPosition.draggingElement.hidden = false;
+          dragPosition.draggingElement.childNodes[0].hidden = false;
+          // dragPosition.draggingElement.childNodes[0].forEach((element) => {
+          //   element.hidden = false;
+          // });
           setPosition((dragPosition) => ({
             ...dragPosition,
             isDragging: true,
@@ -45,8 +72,9 @@ export const ScheduleDragPiece: React.FC<{ part?: { [key: string]: any } }> = ({
               y: clientY - dragPosition.origin.y,
             },
             moveScroll:
-              clientY >= window.innerHeight * 0.75 ||
-              clientY <= window.innerHeight / 3,
+              clientY >= window.innerHeight * 0.7 ||
+              clientY <= window.innerHeight * 0.38,
+            elementBelow,
           }));
         } else {
           setPosition({
@@ -55,6 +83,8 @@ export const ScheduleDragPiece: React.FC<{ part?: { [key: string]: any } }> = ({
             translation: { y: 0 },
             initialScrollTop: 0,
             moveScroll: false,
+            elementBelow: null,
+            draggingElement: null,
           });
         }
       }
@@ -71,19 +101,22 @@ export const ScheduleDragPiece: React.FC<{ part?: { [key: string]: any } }> = ({
       translation: { y: 0 },
       initialScrollTop: 0,
       moveScroll: false,
+      elementBelow: null,
+      draggingElement: null,
     });
   };
 
-  const autoScroll: (direction?: string, element?: HTMLElement) => void = (
-    direction,
-    element
-  ) => {
-    if (element) {
-      direction === "up"
-        ? setInterval(() => element.scrollBy(0, -1), 10)
-        : setInterval(() => element.scrollBy(0, 1), 10);
-    }
-  };
+  // const autoScroll: (direction: string) => void = (direction) => {
+  //   direction === "up"
+  //     ? useInterval(() => {
+  //         document
+  //           ? document.getElementById("innerGrid").scrollBy(0, -1)
+  //           : null;
+  //       }, 10)
+  //     : useInterval(() => {
+  //         document ? document.getElementById("innerGrid").scrollBy(0, 1) : null;
+  //       }, 10);
+  // };
 
   React.useEffect(() => {
     if (dragPosition.isDragging) {
@@ -97,34 +130,32 @@ export const ScheduleDragPiece: React.FC<{ part?: { [key: string]: any } }> = ({
     return window.removeEventListener("touchstart", handleTouchStart);
   }, []);
 
-  React.useEffect(() => {
-    const innerGrid: HTMLElement = document.getElementById("innerGrid");
+  // React.useEffect(() => {
+  //   const innerGrid: HTMLElement = document.getElementById("innerGrid");
 
-    if (dragPosition.moveScroll) {
-      // dragPosition.translation.y > 0
-      //   ? autoScroll("down", innerGrid)
-      //   : autoScroll("up", innerGrid);
-    } else {
-      innerGrid.scrollTop = dragPosition.initialScrollTop;
-    }
-  }, [dragPosition.moveScroll]);
+  //   if (dragPosition.moveScroll) {
+  //     dragPosition.translation.y > 0 ? autoScroll("down") : autoScroll("up");
+  //   } else {
+  //     innerGrid.scrollTop = dragPosition.initialScrollTop;
+  //   }
+  // }, [dragPosition.moveScroll]);
 
   return (
     <div
       className={css.dragWrapper}
       style={{
         height,
+        width: dragPosition.isDragging ? "calc(100% + 2.5%)" : "100%",
+        // marginRight: dragPosition.isDragging ? "5%" : null,
         position: dragPosition.isDragging ? "absolute" : "relative",
         transform: `translate(0, ${dragPosition.translation.y}px)`,
         zIndex: dragPosition.isDragging ? 4 : null,
       }}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
     >
       <button className={css.extendHandle}></button>
-      <div
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
-        className={css.imageBackground}
-      ></div>
+      <div className={css.imageBackground}></div>
       <button className={css.extendHandle}></button>
     </div>
   );
