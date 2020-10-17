2fe54339c2512fa935610200b980fcb89062bf9c
Grant <gmontgomery1994@gmail.com>
2020-10-17 14:09:26 -0700

    some styling for drag and drop.

--- components/ScheduleDragPiece/ScheduleDragPieceDisplay.tsx
+++ components/ScheduleDragPiece/ScheduleDragPieceDisplay.tsx
@@ -13,10 +13,10 @@ export const ScheduleDragPieceDisplay: React.FC<{
       className={css.dragWrapper}
       style={{
         height,
-        width: isDragging ? "calc(100% + 2.5%)" : "100%",
         // marginRight: dragPosition.isDragging ? "5%" : null,
         position: isDragging ? "absolute" : "relative",
         transform: `translate(0, ${translateY}px)`,
+        right: isDragging ? "2.5%" : "0",
         zIndex: isDragging ? 4 : null,
       }}
       onTouchStart={handleTouchStart}
