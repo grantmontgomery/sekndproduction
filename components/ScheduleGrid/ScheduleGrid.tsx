8bc99eaef0bdc14266c2d7345c506177631ebf16
Grant <gmontgomery1994@gmail.com>
2020-10-19 17:00:07 -0700

    part is removed if it doesn't have a name attached when grid is unmounted.

--- components/ScheduleGrid/ScheduleGrid.tsx
+++ components/ScheduleGrid/ScheduleGrid.tsx
@@ -2,21 +2,31 @@ import * as React from "react";
 import { GridHour } from "./GridHour";
 import { GridRectangle } from "./GridRectangle";
 import { useTouchState, useTouchDispatch } from "./Context";
-import { useRectanglesState } from "../../state/GridRectanglesContext";
+import {
+  useRectanglesState,
+  useRectanglesDispatch,
+} from "../../state/GridRectanglesContext";
 import { useGridState } from "../../state/SearchGridContext";
 import css from "./ScheduleGrid.module.scss";
 
 export const ScheduleGrid: React.FC = () => {
-  // const [scroll, enableScroll] = React.useState<boolean>(false);
   const { gridTemplate, hourStrings } = useGridState();
+
   const { rectangles } = useRectanglesState();
+  const rectangleDispatch = useRectanglesDispatch();
   const { stopTouchScroll, customPiece } = useTouchState();
   const touchDispatch = useTouchDispatch();
 
   React.useEffect(() => {
     return () => {
-      console.log("set to false");
-      touchDispatch({ type: "SET_CUSTOM_PIECE" });
+      rectangles.forEach((rectangle, index) => {
+        if (rectangle.part && !rectangle.part.name)
+          rectangleDispatch({
+            type: "REMOVE_PART_FROM_RECTANGLE",
+            payload: { index },
+          });
+      });
+      touchDispatch({ type: "REMOVE_CUSTOM_PIECE" });
     };
   }, []);
   return (
