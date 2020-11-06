ef848a222a1d4c0f9e4e15b521d51793bcfca65a
Grant <gmontgomery1994@gmail.com>
2020-11-05 23:49:49 -0800

    started creating homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import { Layout } from "../components";
+import css from "../styles/Home.module.scss";
 import styles from "../styles/Home.module.css";
 import { useGridState } from "../state/SearchGridContext";
 
-export default function Home() {
-  return <Layout></Layout>;
+export default function Home(): JSX.Element {
+  return (
+    <Layout>
+      <main className={css.homePage}>
+        <header>
+          <h1>You've got the match, now set the perfect date.</h1>
+        </header>
+        <section className={css.walkThrough}></section>
+      </main>
+    </Layout>
+  );
 }
