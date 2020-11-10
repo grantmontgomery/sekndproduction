6c1625a46f1042f09e41b0064e765445e453185b
Grant <gmontgomery1994@gmail.com>
2020-11-09 16:37:35 -0800

    search section basically done for mobile. just need to work on font-sizing and also added select section.

--- components/SelectFeature/SelectFeature.tsx
+++ components/SelectFeature/SelectFeature.tsx
@@ -1,1 +1,14 @@
 import * as React from "react";
+
+export const SelectFeature: React.FC = () => {
+  const [elements, loadElements] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    setTimeout(() => loadElements(true), 250);
+    return clearTimeout;
+  }, []);
+  return (
+    <React.Fragment>
+      <h2>2. Select</h2>
+    </React.Fragment>
+  );
+};
