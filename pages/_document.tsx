925bfcc20ce6ccf2e6a14d5438587eaafe05e3a0
Grant <gmontgomery1994@gmail.com>
2020-11-11 16:30:13 -0800

    SEO add ons.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,29 @@ export default class CustomDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta content="Description goes here." />
+          <meta
+            name="description"
+            content="You've got the match, now set the perfect date. Find the best spots and events in town all in one place."
+          />
+          <meta
+            name="author"
+            content="Grant Montgomery 2020 | grantcreates.com"
+          ></meta>
+          <meta
+            name="keywords"
+            content="Seknd, Grant Montgomery, Make a Date, Grant Creates, React App"
+          ></meta>
+          <meta
+            name="og:description"
+            content="You've got the match, now set the perfect date. Find the best spots and events in town all in one place."
+          ></meta>
+          <meta
+            name="og:image"
+            title="main logo"
+            content="/images/SekndSEOPic.png"
+          ></meta>
+          <meta name="og:title" content="Seknd"></meta>
+          <meta name="og:url" content="https://sekndapp.com"></meta>
           <title>Seknd</title>
           <link rel="icon" href="/favicon.ico" />
           <script
