3c55318d0449e2e795926d360dd03b3d5d4f8d17
Grant <gmontgomery1994@gmail.com>
2020-11-16 17:27:36 -0800

    adding a 404 page.

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import css from "../styles/404.module.scss";
+import { Layout } from "../components";
+
+export default function Custom404(): JSX.Element {
+  return (
+    <Layout>
+      <main className={css.errorPage}>
+        <section>
+          <h1>Oops!</h1>
+          <h2>This page doesn't exist.</h2>
+        </section>
+      </main>
+    </Layout>
+  );
+}
