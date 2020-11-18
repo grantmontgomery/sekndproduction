19834c3bd919c8017bf21b16fc87099769ce0ce3
Grant <gmontgomery1994@gmail.com>
2020-11-18 13:02:47 -0800

    sign in register input styling set up.

--- components/SignInModal/SignInModal.tsx
+++ components/SignInModal/SignInModal.tsx
@@ -7,15 +7,41 @@ export const SignInModal: React.FC = () => {
   return (
     <div className={css.signInWrapper}>
       <div className={css.signInSelector}>
-        <span>Sign in</span>
-        <span>Sign up</span>
+        <span
+          onClick={() => setMode("signin")}
+          style={{
+            borderBottom: mode === "signin" ? "1px solid black" : "none",
+          }}
+        >
+          Sign in
+        </span>
+        <span
+          onClick={() => setMode("register")}
+          style={{
+            borderBottom: mode === "register" ? "1px solid black" : "none",
+          }}
+        >
+          Register
+        </span>
       </div>
-      <input type="text" placeholder="Username or Email" name="" id="" />
-      <input type="password" placeholder="Password" />
-      <span className={css.rememberPassword}>
-        <input type="checkbox" /> Remember Password
-      </span>
-      <span className={css.loginButton}>Log in</span>
+      {mode === "signin" ? (
+        <React.Fragment>
+          <input type="text" placeholder="Username or Email" name="" id="" />
+          <input type="password" placeholder="Password" />
+          <span className={css.rememberPassword}>
+            <input type="checkbox" />
+            Remember Password
+          </span>
+          <span className={css.loginButton}>Log in</span>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <input type="text" placeholder="Username" name="" id="" />
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <span className={css.registerButton}>Register</span>
+        </React.Fragment>
+      )}
     </div>
   );
 };
