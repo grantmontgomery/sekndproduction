61e55ac55dab3b899b029c7bfca544444af1afd8
Grant <gmontgomery1994@gmail.com>
2020-11-17 19:51:56 -0800

    added a remember password button and also going to have buttons specifying a sign up or sign in.

--- components/SignInModal/SignInModal.tsx
+++ components/SignInModal/SignInModal.tsx
@@ -2,10 +2,19 @@ import * as React from "react";
 import css from "./SignInModal.module.scss";
 
 export const SignInModal: React.FC = () => {
+  const [mode, setMode] = React.useState<string>("signin");
+
   return (
     <div className={css.signInWrapper}>
+      <div className={css.signInSelector}>
+        <span>Sign in</span>
+        <span>Sign up</span>
+      </div>
       <input type="text" placeholder="Username or Email" name="" id="" />
       <input type="password" placeholder="Password" />
+      <span className={css.rememberPassword}>
+        <input type="checkbox" /> Remember Password
+      </span>
       <span className={css.loginButton}>Log in</span>
     </div>
   );
