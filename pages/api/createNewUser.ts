57175347f5599800066261bdd2470862b1644567
Grant <gmontgomery1994@gmail.com>
2020-11-20 15:28:46 -0800

    creating test queries with graphql.

--- pages/api/createNewUser.ts
+++ pages/api/createNewUser.ts
@@ -1,9 +1,29 @@
 import { ApolloServer, gql, makeExecutableSchema } from "apollo-server-micro";
-import { typeDefs, resolvers } from "../../graphqlSetUp/schemas";
+// import { typeDefs, resolvers } from "../../graphqlSetUp/schemas";
 
 export const config = {
   api: {
     bodyParser: false,
+  },
+};
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+  }
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    password: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users() {
+      return;
+    },
   },
 };
 
