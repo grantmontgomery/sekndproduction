f5f0d5a79c46da66887df1bf9b5013f1b65c8ad2
Grant <gmontgomery1994@gmail.com>
2020-11-20 14:23:26 -0800

    test configuration for rds mysql database.

--- next.config.original.1602525802864.js
+++ next.config.original.1602525802864.js
@@ -5,6 +5,10 @@ module.exports = {
     YELP_API_KEY: process.env.YELP_API_KEY,
     TICKETMASTER_API_KEY: process.env.TICKETMASTER_API_KEY,
     GA_TRACKING_ID: process.env.GA_TRACKING_ID,
+    DB_ENDPOINT: process.env.DB_ENDPOINT,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
   },
   serverRuntimeConfig: {
     YELP_API_KEY: process.env.YELP_API_KEY,
