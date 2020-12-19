f9ad8465fb950860d4e62af09fe1361244233118
Grant <gmontgomery1994@gmail.com>
2020-12-18 21:44:02 -0800

    setting up separate api calls for ticketmaster and yelp.

--- components/ResultsSection/Hooks/useEventsCall.tsx
+++ components/ResultsSection/Hooks/useEventsCall.tsx
@@ -1,16 +1,27 @@
-// const useEvents = () => {
-//     const [eventsLoading, setLoading] = React.useState<boolean>(false)
+const url:"https://sekndapp.com" | "http://localhost:3000" = process.env.NODE_ENV === "development" ? "http://localhost:3000"
+: "https://sekndapp.com"
 
-//     const triggerEventsCall:(searchParams: {
-//         [key: string]: any;
-//       }) =>  Promise<{ [key: string]: any }[] | string> = async () => {
-//           setLoading(true)
+export const useEvents = () => {
+    const [eventsLoading, setLoading] = React.useState<boolean>(false)
 
-//           try{
+    const triggerEventsCall:(searchParams: {
+        [key: string]: any;
+      }) =>  Promise<{ [key: string]: any }[] | string> = async () => {
+          setLoading(true)
 
-//           }
-//           catch{
+          try{
+            const { location, radius, placeType, placesPrice, offset } = searchParams;
+            const response:Response = await fetch(`${url}/api/`, {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                method: 
+            })
 
-//           }
-//       }
-// }
+          }
+          catch{
+
+          }
+      }
+}
---
+++ components/ResultsSection/Hooks/useTicketMasterCall.tsx
@@ -0,0 +1,14 @@
+// const url:"http://localhost:3000"|"https://sekndapp.com" = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sekndapp.com"
+
+// export const useTicketMasterCall:() => any = async() => {
+//    const [ticketmasterLoading, setLoading] = React.useState<boolean>(false)
+//     const triggerTicketMasterCall:() => Promise<{[key:string]:any}[]|string> = async () => {
+//         try{
+//             fetch(`${url}/api/ticketmasterAPI`)
+//         }
+//         catch(error){
+
+//         }
+//     }
+//     return {ticketmasterLoading, triggerTicketMasterCall}
+// }
