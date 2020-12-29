334452fcd68bf045d068cdac53b27deb881f48f9
Grant <gmontgomery1994@gmail.com>
2020-12-28 22:12:58 -0800

    moved mobile filters to the results filter component.

--- pages/search/[queried].tsx
+++ pages/search/[queried].tsx
@@ -140,7 +140,6 @@ export default function Queried(): JSX.Element {
           </CSSTransition>
         ) : null}
       </TransitionGroup> */}
-        <MobileFilters></MobileFilters>
       </main>
     </Layout>
   );
