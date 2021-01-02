15b2a467757bf2ecf362f04c6e18f0eb479965e1
Grant <gmontgomery1994@gmail.com>
2021-01-01 23:04:12 -0800

    environmental variables.

--- next.config.original.1602525802864.js
+++ next.config.original.1602525802864.js
@@ -1,22 +1,6 @@
 require(`dotenv`).config();
 
 module.exports = {
-  env: {
-    YELP_API_KEY: process.env.YELP_API_KEY,
-    TICKETMASTER_API_KEY: process.env.TICKETMASTER_API_KEY,
-    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
-    DB_ENDPOINT: process.env.DB_ENDPOINT,
-    DB_DATABASE: process.env.DB_DATABASE,
-    DB_TABLE: process.env.DB_TABLE,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    SESSION_SECRET: process.env.SESSION_SECRET,
-  },
-  serverRuntimeConfig: {
-    YELP_API_KEY: process.env.YELP_API_KEY,
-    TICKETMASTER_API_KEY: process.env.TICKETMASTER_API_KEY,
-    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
-  },
   target: "serverless",
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
